31dd4eb19705731003007d38f7f9b858eac2b96e
初旭 <chuxu@chuxudeMacBook-Pro.local>
2017-04-02 15:14:40 -0400

    creat the header and footer

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router
 
-module.exports = router;
+
+.get('/', function(req, res, next) {
+		  res.render('index', {
+		  	title: "123",
+		   partials: {
+		   	header: './partials/header',
+		  	footer: './partials/footer'
+		  }
+		})
+	})
+module.exports = router
